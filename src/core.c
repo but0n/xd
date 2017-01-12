48f9e3914a4a0374bf654394cd2ae60706d325cd
but0n <but0n@icloud.com>
2017-01-13 00:33:49 +0800

    feat: Read file

--- src/core.c
+++ src/core.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 
 
+void readImg(const char *filename) {
+	FILE *fp = NULL;
+	fp = fopen(filename, "rb");
+	if (fp==NULL)
+		printf("Failed\n");
+	fclose(fp);
+}
+
+
 int main() {
+	readImg("logo.png");
 	printf("Hello\n");
 	return 0;
 }
