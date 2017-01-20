8d3969ce7c155d937d8e08e6031ecb1fe6c8afba
but0n <but0n@icloud.com>
2017-01-20 14:00:44 +0800

    feat: xxd

--- src/core.c
+++ src/core.c
@@ -13,17 +13,32 @@ void readPNG(const char *filename) {
 		printf("Failed\n");
 	else {
 		printf("Reading...\n");
-		int magic_number = 0;
-		int number = 0;
-		fread((char*)&magic_number, sizeof(magic_number), 1, fp);
-		magic_number = ReverseInt(magic_number);
-		printf("Magic number:\t0x%08x\n", magic_number);
-		fread((char*)&number, sizeof(number), 1, fp);
-		number = ReverseInt(number);
-		printf("Number:\t0x%08x\n", number);
+		// int magic_number = 0;
+		// int number = 0;
+		// fread((char*)&magic_number, sizeof(magic_number), 1, fp);
+		// magic_number = ReverseInt(magic_number);
+		// printf("Magic number:\t0x%08x\n", magic_number);
+		// fread((char*)&number, sizeof(number), 1, fp);
+		// number = ReverseInt(number);
+		// printf("Number:\t0x%08x\n", number);
+		int cache;
+		int size = 0;
+		while(fread((char*)&cache, sizeof(cache), 1, fp)) {
+			unsigned char *split = (unsigned char*)&cache;
+			if(!(size%16)) {
+				printf("\n");
+				printf("%08x: ", size);
+			}
+			printf("%02x ", split[0]);
+			printf("%02x ", split[1]);
+			printf("%02x ", split[2]);
+			printf("%02x  ", split[3]);
+			size+=4;
+		}
+		printf("\n%d Byte total\n", size);
 	}
-	fp = NULL;
 	fclose(fp);
+	fp = NULL;
 }
 
 
